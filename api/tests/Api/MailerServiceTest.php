da95c0086c840e4fa0240bfd636747f055c86206
LouisAntoine <louisantoine.pro@gmail.com>
2024-01-31 22:03:50 +0100

    Update tests

--- api/tests/Api/MailerServiceTest.php
+++ api/tests/Api/MailerServiceTest.php
@@ -2,16 +2,18 @@
 
 use PHPUnit\Framework\TestCase;
 use App\Service\MailerService;
+use GuzzleHttp\Client;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Model\SendSmtpEmail;
 
 class MailerServiceTest extends TestCase
 {
     public function testSendEmail()
     {
-        $clientMock = $this->getMockBuilder(\GuzzleHttp\Client::class)->getMock();
-        $configMock = $this->getMockBuilder(\SendinBlue\Client\Configuration::class)->getMock();
-        $apiInstanceMock = $this->getMockBuilder(\SendinBlue\Client\Api\TransactionalEmailsApi::class)
-            ->setConstructorArgs([$clientMock, $configMock])
-            ->getMock();
+        $clientMock = $this->getMockBuilder(Client::class)->getMock();
+        $configMock = $this->getMockBuilder(Configuration::class)->getMock();
+        $apiInstanceMock = $this->getMockBuilder(TransactionalEmailsApi::class)->disableOriginalConstructor()->getMock();
 
         $options = [
             'emailTo' => 'test@example.com',
@@ -23,7 +25,7 @@ class MailerServiceTest extends TestCase
         $configMock->method('setApiKey')->willReturnSelf();
         $clientMock->expects($this->once())->method('__call')->willReturn($apiInstanceMock);
 
-        $expectedSendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail();
+        $expectedSendSmtpEmail = new SendSmtpEmail();
 
         $apiInstanceMock->expects($this->once())->method('sendTransacEmail')->willReturn($expectedSendSmtpEmail);
     }
